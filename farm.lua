84b706e13f06c7f4cd533899e4456d4eb7510871
Iilia Jayjock <jayjocii@students.pittschools.org>
2020-07-22 14:19:57 -0400

    fdskal;rqhyanjir;pnfvdjopA

--- farm.lua
+++ farm.lua
@@ -7,13 +7,14 @@ when inventory full put crops in chest
 when out of bone meal get more from chest]]
 
 
-
-
-
-
-
-
+function place bone meal
+  cont=true()
+  while cont do
+  if turtle
 
 
 
 turtle.dig()
+turtle.select(2)
+turtle.place()
+turtle.select(1)
