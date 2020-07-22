5b1d28a243c546ab66b77a180fa08b916ddafb9f
Iilia Jayjock <jayjocii@students.pittschools.org>
2020-07-22 14:49:59 -0400

    functon added

--- farm.lua
+++ farm.lua
@@ -1,20 +1,25 @@
 --[[hoe space to plant seeds
 plant seeds
-bone meal
+apply bone meal until fully grown
 chop plant to get drops
 repeat to farm the crops and get drops from the plant
 when inventory full put crops in chest
 when out of bone meal get more from chest]]
 
 
-function place bone meal
+function BoneMeal()
   cont=true()
   while cont do
-  if turtle
-
+  turtle.place()
+ else
+  cont=false
+end
+ end
+  end
 
 
 turtle.dig()
 turtle.select(2)
 turtle.place()
 turtle.select(1)
+BoneMeal()
